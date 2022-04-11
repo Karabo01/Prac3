5c965ad51504d6529bc5099d98babc8b47c11afa
Karabo <morena.awdtech@gmail.com>
2022-04-11 18:09:33 +0200

    Added some functions for data handling

--- AWDBOT.py
+++ AWDBOT.py
@@ -6,7 +6,7 @@ np.random.seed(0)
 
 import sys
 
-def progressbar(it, prefix="", size=60, file=sys.stdout):
+def progressbar(it, prefix="", size=60, file=sys.stdout):#Progress bar for indicating training progress
     count = len(it)
     def show(j):
         x = int(size*j/count)
@@ -20,15 +20,15 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
     file.flush()
 
 
-class Layer:
-    def _init_(self, num_inputs,num_neurons):
+class Layer:# Class defines different layers in NN
+    def _init_(self, num_inputs,num_neurons):#initializes random weights and Biases
         self.weights = 0.10*np.random(num_inputs,num_neurons)
         self.biases = np.zeros((1, num_neurons))
 
     def forwardPass(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-class Activation_RELU:
+class Activation_RELU: # Activation class for neurons
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
@@ -42,7 +42,7 @@ def reConvert(val):#Converts integers into RPS
     else:
         return 'S'
 
-def convert(Char):#Converts RPS into integers
+def convert(Char):#Converts RPS From Yt into desired Xt
     if Char=="R":
         return "P"
     if Char=="P":
@@ -50,16 +50,16 @@ def convert(Char):#Converts RPS into integers
     else:
         return "R"
 
-def dataExtraction():
+def dataExtraction():# Extracts data from CSV File
     sequence_filename = "data1.csv"
     #for i in progressbar(range(100), "Computing: ", 100):
     with open(sequence_filename, newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
     print("Data extracted!!")
-    return data
+    return data#
 
-def arrange(yt,xt):
+def arrange(yt,xt):#Separates Yt and Xt into separate variables
     for i in range(len(data)):
         yt.append(str(data[i])[10:11])
         xt.append(str(data[i])[2:6])
@@ -69,4 +69,29 @@ yt=[]
 xt=[]
 arrange(yt,xt)
 
+def checkOneHot(Char):
+    if Char=='R':
+        return 0
+    elif Char=='P':
+        return 1
+    else:
+        return 2
+
+def getInputs(xt):
+    X=[]
+    for i in range(int(0.001*len(xt))):
+        X.append([])
+        for j in range(7):
+            X[i].append(0)
+    for i in range(int(0.001*len(xt))):
+        play=str(xt[i][1:2])
+        index=checkOneHot(play)
+        X[i][index]=="heloo"#for some reason thisdoesnt work
+
+"""
+layer1= Layer(2,7)#Creates layer 1, 2 features and 7 neurons
+activation1=Activation_RELU() #activation for layer 1(Input layer)
+layer1.forwardPass(X)
+"""
+getInputs(xt)
 print("hello")
